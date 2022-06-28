84e8ed8c005d8fce1971a1f7c94f1497706eee12
noah gaston <noahsfuture18@gmail.com>
2022-06-28 15:55:52 -0400

    question set up

---
+++ src/Dragon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Dragon {
+    public static void main(String[] args) {
+//        System.out.println("hello");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Which cave will you enter?(1 or 2");
+        String cave = scan.next();
+
+
+    }
+}
